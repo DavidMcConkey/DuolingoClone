42ef9c45c6e1f85ca2ba825c865e912bd5ed294d
David McConkey <davidm@Davids-MacBook-Air.local>
2023-08-25 12:46:46 -0400

    create base about page

---
+++ app/pages/about.tsx
@@ -0,0 +1,26 @@
+export default () => (
+    <div className="columns is-mobile is-centered">
+      <div className="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
+          <section className="hero is-medium">
+              <div className="hero-body">
+                  <div className="container">
+                  <h1 className="title">
+                      About Page
+                  </h1>
+                  <h2 className="subtitle">
+                    EyeGym is maintained by <a href="https://github.com/DavidMcConkey"> David McConkey </a>
+                  </h2>
+                  <p>
+                    
+                  </p>
+                  <p>
+                    Inspired from <a href="https://www.mikealche.com/software-development/how-to-implement-authentication-in-next-js-without-third-party-libraries" >an article</a> of Mike Alche.
+                  </p>
+    
+                  <p>Built with the great <a href="https://github.com/vvo/next-iron-session">Next-iron-session</a> backend utility to manage client sessions.</p>
+                  </div>
+              </div>
+          </section>
+      </div>
+    </div>
+    )
