c3f571fb4e14ae9c79822061f7a9a9975e46016a
David McConkey <davidm@Davids-MacBook-Air.local>
2023-08-25 10:02:34 -0400

    implement base footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default () => {
+    <footer className="footer">
+        <div className="content has-text-centered">
+            Powered by
+            <Link href="/about">
+                <a>
+                    Next / Auth
+                </a>
+            </Link>
+            <p><a href="https://github.com/DavidMcConkey"><i className="fab fa-github"></i></a></p>
+        </div>
+    </footer>
+}
